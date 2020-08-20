53f603fdaf87119047aee4b6339c617e3e65f9cc
Muhamad Hilman <muhammadh@thebigrichgroup.com>
2020-08-21 02:47:16 +0700

    halaman blog desa semua user

--- application/views/user/footer.php
+++ application/views/user/footer.php
@@ -20,10 +20,11 @@
 
 
 <!-- Bootstrap core JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- Third party plugin JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
 <!-- Contact form JS-->
 <script src="<?= base_url('assets/'); ?>assets/mail/jqBootstrapValidation.js"></script>
 <script src="<?= base_url('assets/'); ?>assets/mail/contact_me.js"></script>
--- application/views/user/home.php
+++ application/views/user/home.php
@@ -15,8 +15,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#profile">Profile Desa</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Program Desa</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Sejarah</a></li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Penduduk</a></li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a></li>
             </ul>
@@ -49,25 +48,35 @@
 <header class="masthead bg-primary text-white text-center">
     <div class="container d-flex align-items-center flex-column">
         <!-- Masthead Avatar Image-->
-        <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="" />
+        <img class="masthead-avatar mb-5" src="<?= base_url('assets/'); ?>assets_m/img/icon-desa.png" alt="" />
         <!-- Masthead Heading-->
         <h1 class="masthead-heading text-uppercase mb-0">Desa Bojongkeding</h1>
         <!-- Icon Divider-->
 
         <!-- Masthead Subheading-->
-        <p class="masthead-subheading font-weight-light mb-0">Profile Desa Bojongkeding</p>
+        <p class="masthead-subheading font-weight-light mb-0">Berani (Bersih, Energik, Ramah, Aman, Nyaman, Indah)</p>
     </div>
 </header>
 <!-- Portfolio Section-->
 <section class="page-section portfolio" id="portfolio">
     <div class="container">
         <!-- Portfolio Section Heading-->
-        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Program Desa Bojongkeding</h2>
-        <!-- Icon Divider-->
-        <div class="divider-custom">
-            <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-            <div class="divider-custom-line"></div>
+        <h2 class="page-section-subheading  text-secondary mb-0">SEJARAH</h2>
+        <div class="mt-4">
+            <h5>A. KONDISI DESA BOJONGKEDING</h5>
+            <p>Desa Bojongkeding merupakan salah satu jajaran sembilan desa agrari yang ada di Kecamatan Tambakdahan Kabupaten Subang, yang terletak disebelah Timur sebelum desa bojonegara dengan luas 491.566 Ha yang dikelilingi oleh hamparan lahan pertanian dan terbentang sungai yang melilinginya.</p>
+            <h5>B. SEJARAH DESA BOJONGKEDING</h5>
+            <p>Bukti adanya kelompok masyarakat pada masa prasejarah di wilayah Kabupaten Subang adalah ditemukannya kapak batu di daerah Bojongkeding (Tambakdahan), Pagaden, Kalijati dan Dayeuhkolot (Sagalaherang). Temuan benda-benda prasejarah bercorak neolitikum ini menandakan bahwa saat itu di wilayah Kabupaten Subang sekarang sudah ada kelompok masyarakat yang hidup dari sektor pertanian dengan pola sangat sederhana.
+                Selain itu, dalam periode prasejarah juga berkembang pula pola kebudayaan perunggu yang ditandai dengan penemuan situs di Kampung Engkel, Sagalaherang.
+
+            </p>
+            <p>
+                Desa Bojongkeding berada di sebelah timur kota kecamatan tambakdahan, Menurut sumber yang kami dapatkan Desa Bojongkeding dibentuk pada tahun 1920 itu dibuktikan dengan ditemukannya kapak batu jaman prasejarah di desa bojongkeding, artinya bahwa diDesa bojongkeding sudah ada kelompok masyarakat pada masa itu.
+                Menurut sumber yang dapat kami uraikan BOJONGKEDING berasal dari dua kata yaitu “BOJO” Artinya istri dan “KEDING” merupakan nama sebuah kali yang kecil yang terletak ditengah-tengah desa,konon katanya kali itu sebagai sumber mata air yang dapat digunakan oleh masyarakat untuk kehidupan sehari-hari baik untuk keperluan keluarga juga untuk keperluan pertanian. Hamparan sawah yang luas merupakan salah satu asset kekayaan yang dimiliki oleh desa bojongkeding sebagai modal untuk membangun desa.
+            </p>
+            <p>
+                Mayoritas mata pencaharian masyarakat desa bojongkeding adalah petani banyak ritual yang dilakukan masyarakat dalam bercocok tanam padi, mulai dari ritual turun kesawah dinamakan sedekah bumi yang tujuan nya agar selama penanaman padi hingga panen hasil yang diharapkan meningkat dan tidak ada kendala seperti serangan hama, penyemaian benih padi sampai pemotongan pun ritual itu dilakukan dengan cara sedekah bumi yang lebih dikenal oleh masyarakat desa bojongkeding adalah “ mapag sri” (menyambut kedatangan sang dei padi,red), yang tujuannya adalah rasa syukur kepada sang pencipta karna hasil panen petani meningkat kata lain tidak ada kegagalan dalam bercocok tanam padi didesa bojongkeding ritual-ritual tersebut sudah menjadi budaya yang sangat kental dilakukan oleh masyarakat sampai saat ini yang tak mungkin bias dihilangkan dengan iringan kesenian tradisional wayang kulit yang isi ceritanya tentang sejarah dewi sri sang dewi padi.
+            </p>
         </div>
         <!-- Portfolio Grid Items-->
         <div class="row">
@@ -75,7 +84,7 @@
             <div class="col-md-6 col-lg-4 mb-5">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                        <div class="portfolio-item-caption-content text-center text-dark"><i class="fas fa-plus fa-3x"></i></div>
                     </div>
                     <img class="img-fluid" src="assets/img/portfolio/cabin.png" alt="" />
                 </div>
@@ -84,7 +93,7 @@
             <div class="col-md-6 col-lg-4 mb-5">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                        <div class="portfolio-item-caption-content text-center text-dark"><i class="fas fa-plus fa-3x"></i></div>
                     </div>
                     <img class="img-fluid" src="assets/img/portfolio/cake.png" alt="" />
                 </div>
@@ -98,35 +107,6 @@
                     <img class="img-fluid" src="assets/img/portfolio/circus.png" alt="" />
                 </div>
             </div>
-            <!-- Portfolio Item 4-->
-            <div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
-                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal4">
-                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                    </div>
-                    <img class="img-fluid" src="assets/img/portfolio/game.png" alt="" />
-                </div>
-            </div>
-            <!-- Portfolio Item 5-->
-            <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
-                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal5">
-                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                    </div>
-                    <img class="img-fluid" src="assets/img/portfolio/safe.png" alt="" />
-                </div>
-            </div>
-            <!-- Portfolio Item 6-->
-            <div class="col-md-6 col-lg-4">
-                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal6">
-                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                    </div>
-                    <img class="img-fluid" src="assets/img/portfolio/submarine.png" alt="" />
-                </div>
-            </div>
-        </div>
-    </div>
 </section>
 <!-- About Section-->
 <section class="page-section bg-primary text-white mb-0" id="about">
@@ -165,8 +145,6 @@
         <!-- Icon Divider-->
         <div class="divider-custom">
             <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-            <div class="divider-custom-line"></div>
         </div>
         <!-- Contact Section Form-->
         <div class="row">
@@ -175,29 +153,29 @@
                 <form id="contactForm" name="sentMessage" novalidate="novalidate">
                     <div class="control-group">
                         <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                            <label>Nama Lengkap</label>
-                            <input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name." />
+                            <!-- <label>Nama Lengkap</label> -->
+                            <input class="form-control" id="name" type="text" placeholder="Nama Lengkap" required="required" data-validation-required-message="Please enter your name." />
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
                     <div class="control-group">
                         <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                            <label>Email</label>
-                            <input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address." />
+                            <!-- <label>Email</label> -->
+                            <input class="form-control" id="email" type="email" placeholder="Email" required="required" data-validation-required-message="Please enter your email address." />
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
                     <div class="control-group">
                         <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                            <label>Nomor Telp/HP</label>
+                            <!-- <label>Nomor Telp/HP</label> -->
                             <input class="form-control" id="phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number." />
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
                     <div class="control-group">
                         <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                            <label>Pesan</label>
-                            <textarea class="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>
+                            <!-- <label>Pesan</label> -->
+                            <textarea class="form-control" id="message" rows="5" placeholder="Tulis Pesan..." required="required" data-validation-required-message="Please enter a message."></textarea>
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
@@ -213,32 +191,24 @@
 <footer class="footer text-center">
     <div class="container">
         <div class="row">
-            <!-- Footer Location-->
-            <div class="col-lg-4 mb-5 mb-lg-0">
-                <h4 class="text-uppercase mb-4">Location</h4>
+            <!-- Footer Lokasi-->
+            <div class="col-lg-6 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Lokasi</h4>
                 <p class="lead mb-0">
-                    2215 John Daniel Drive
+                    Jl. Sudirman No 107 Bojongkeding, Tambakdahan Kabupaten Subang
                     <br />
-                    Clark, MO 65243
+                    Jawa Barat - 41253
                 </p>
             </div>
             <!-- Footer Social Icons-->
-            <div class="col-lg-4 mb-5 mb-lg-0">
-                <h4 class="text-uppercase mb-4">Around the Web</h4>
+            <div class="col-lg-6 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Follow</h4>
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
             </div>
-            <!-- Footer About Text-->
-            <div class="col-lg-4">
-                <h4 class="text-uppercase mb-4">About Freelancer</h4>
-                <p class="lead mb-0">
-                    Freelance is a free to use, MIT licensed Bootstrap theme created by
-                    <a href="http://startbootstrap.com">Start Bootstrap</a>
-                    .
-                </p>
-            </div>
+
         </div>
     </div>
 </footer>
@@ -295,10 +265,8 @@
                             </div>
                             <div class="divider-custom">
                                 <div class="divider-custom-line"></div>
-                                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                                <div class="divider-custom-line"></div>
                             </div>
-                            <div id="profileModal1Label"><img class="rounded-circle" width="200px" src="<?= base_url('assets/img/profile/') . $user['image']; ?>"></div>
+                            <div id="profileModal1Label"><img class="img-thumbnail" width="300px" src="<?= base_url('assets/img/profile/') . $user['image']; ?>"></div>
                             <table width="100%">
                                 <td class="col-md-8 justify-content-center">
                                     <table width="100%" style="padding-left: 2px; padding-right: 13px;" class="table my-5">
@@ -334,7 +302,7 @@
                             </table>
                             <button class="btn btn-primary" data-dismiss="modal">
                                 <i class="fas fa-times fa-fw"></i>
-                                Close Window
+                                Tutup
                             </button>
                         </div>
                     </div>
